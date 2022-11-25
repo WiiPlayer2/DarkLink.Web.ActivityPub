18dc909031f14d6bcabc4ffc2a2449f00213fdd3
Waldemar Tomme <waldetomme@gmail.com>
2022-11-25 23:13:05 +0100

    Use scheme and host from request

--- Demo.Server/Program.cs
+++ Demo.Server/Program.cs
@@ -65,17 +65,17 @@ app.MapGet("/profiles/{username}.json", async ctx =>
     ctx.Response.Headers.CacheControl = "max-age=0, private, must-revalidate";
     ctx.Response.Headers.ContentType = "application/activity+json; charset=utf-8";
 
-    var person = new Person(new Uri($"https://devtunnel.dark-link.info/profiles/{username}/inbox"), new Uri($"https://devtunnel.dark-link.info/profiles/{username}/outbox"))
+    var person = new Person(new Uri($"{ctx.Request.Scheme}://{ctx.Request.Host}/profiles/{username}/inbox"), new Uri($"{ctx.Request.Scheme}://{ctx.Request.Host}/profiles/{username}/outbox"))
     {
-        Id = new Uri($"https://devtunnel.dark-link.info/profiles/{username}.json"),
+        Id = new Uri($"{ctx.Request.Scheme}://{ctx.Request.Host}/profiles/{username}.json"),
         PreferredUsername = username,
         Name = $"Waldemar Tomme [{username}]",
         Summary = "Just testing around 🧪",
-        Url = DataList.From<LinkOr<ASLink>>(new Link<ASLink>(new Uri($"https://devtunnel.dark-link.info/profiles/{username}"))),
+        Url = DataList.From<LinkOr<ASLink>>(new Link<ASLink>(new Uri($"{ctx.Request.Scheme}://{ctx.Request.Host}/profiles/{username}"))),
         Icon = DataList.From<LinkOr<Image>>(new Object<Image>(new Image
         {
             MediaType = "image/png",
-            Url = DataList.From<LinkOr<ASLink>>(new Link<ASLink>(new Uri("https://devtunnel.dark-link.info/profile.png"))),
+            Url = DataList.From<LinkOr<ASLink>>(new Link<ASLink>(new Uri($"{ctx.Request.Scheme}://{ctx.Request.Host}/profile.png"))),
         })),
     };
 
@@ -93,7 +93,7 @@ app.MapGet("/profiles/{username}/outbox", async ctx =>
     var activities = await new DirectoryInfo($"./data/{username}")
         .EnumerateFiles("*.txt")
         .OrderBy(f => f.CreationTime)
-        .Select(f => GetNoteActivityAsync(username, f.Name, ctx.RequestAborted))
+        .Select(f => GetNoteActivityAsync(ctx.Request.Scheme, ctx.Request.Host.ToString(), username, f.Name, ctx.RequestAborted))
         .WhenAll();
 
     var outboxCollection = new OrderedCollection
@@ -116,7 +116,7 @@ app.MapGet("/notes/{username}/{note}", async ctx =>
         || !ctx.Request.RouteValues.TryGetValue("note", out var noteFileRaw)
         || noteFileRaw is not string noteFile) return;
 
-    var note = await GetNoteAsync(username, noteFile, ctx.RequestAborted);
+    var note = await GetNoteAsync(ctx.Request.Scheme, ctx.Request.Host.ToString(), username, noteFile, ctx.RequestAborted);
     var node = LinkedDataSerializer.Serialize(note, commonContext, jsonOptions);
 
     ctx.Response.Headers.ContentType = "application/activity+json; charset=utf-8";
@@ -131,7 +131,7 @@ app.MapGet("/notes/{username}/{note}/activity", async ctx =>
         || !ctx.Request.RouteValues.TryGetValue("note", out var noteFileRaw)
         || noteFileRaw is not string noteFile) return;
 
-    var activity = await GetNoteActivityAsync(username, noteFile, ctx.RequestAborted);
+    var activity = await GetNoteActivityAsync(ctx.Request.Scheme, ctx.Request.Host.ToString(), username, noteFile, ctx.RequestAborted);
     var node = LinkedDataSerializer.Serialize(activity, commonContext, jsonOptions);
 
     ctx.Response.Headers.ContentType = "application/activity+json; charset=utf-8";
@@ -179,28 +179,28 @@ bool CheckRequest(HttpContext ctx, [NotNullWhen(true)] out string? username)
     return true;
 }
 
-async Task<Note> GetNoteAsync(string username, string filename, CancellationToken cancellationToken = default)
+async Task<Note> GetNoteAsync(string scheme, string host, string username, string filename, CancellationToken cancellationToken = default)
 {
     var fileInfo = new FileInfo($"./data/{username}/{filename}");
     return new Note
     {
-        Id = new Uri($"https://devtunnel.dark-link.info/notes/{username}/{fileInfo.Name}"),
+        Id = new Uri($"{scheme}://{host}/notes/{username}/{fileInfo.Name}"),
         To = DataList.From<LinkOr<Object>>(new Link<Object>(new Uri("https://www.w3.org/ns/activitystreams#Public"))),
-        AttributedTo = DataList.From<LinkOr<Object>>(new Link<Object>(new Uri($"https://devtunnel.dark-link.info/profiles/{username}.json"))),
+        AttributedTo = DataList.From<LinkOr<Object>>(new Link<Object>(new Uri($"{scheme}://{host}/profiles/{username}.json"))),
         //Published = fileInfo.CreationTime,
         Content = await File.ReadAllTextAsync(fileInfo.FullName, cancellationToken),
     };
 }
 
-async Task<Create> GetNoteActivityAsync(string username, string filename, CancellationToken cancellationToken = default)
+async Task<Create> GetNoteActivityAsync(string scheme, string host, string username, string filename, CancellationToken cancellationToken = default)
 {
-    var note = await GetNoteAsync(username, filename, cancellationToken);
+    var note = await GetNoteAsync(scheme, host, username, filename, cancellationToken);
     return new Create
     {
         Id = new Uri($"{note.Id}/activity"),
         //Published = note.Published,
         To = note.To,
-        Actor = DataList.From<LinkTo<Actor>>(new Uri($"https://devtunnel.dark-link.info/profiles/{username}.json")),
+        Actor = DataList.From<LinkTo<Actor>>(new Uri($"{scheme}://{host}/profiles/{username}.json")),
         Object = DataList.From<LinkTo<Object>>(note),
     };
 }
--- Demo.Server/ResourceDescriptorProvider.cs
+++ Demo.Server/ResourceDescriptorProvider.cs
@@ -4,17 +4,15 @@ using DarkLink.Web.WebFinger.Shared;
 
 internal class ResourceDescriptorProvider : IResourceDescriptorProvider
 {
-    public const string USER = "dev2";
-
     public async Task<JsonResourceDescriptor?> GetResourceDescriptorAsync(Uri resource, IReadOnlyList<string> relations, HttpRequest request, CancellationToken cancellationToken)
     {
         if (resource.Scheme is not "acct")
             return default;
-        
-        if (!resource.LocalPath.EndsWith("@devtunnel.dark-link.info"))
+
+        if (!resource.LocalPath.EndsWith($"@{request.Host}"))
             return default;
 
-        var username = resource.LocalPath[..^25];
+        var username = resource.LocalPath[..^request.Host.ToString().Length];
 
         if (!Directory.Exists("./data"))
             Directory.CreateDirectory("./data");
@@ -32,12 +30,12 @@ internal class ResourceDescriptorProvider : IResourceDescriptorProvider
                 Link.Create(Constants.RELATION_PROFILE_PAGE) with
                 {
                     Type = "text/html",
-                    Href = new Uri($"https://devtunnel.dark-link.info/profiles/{username}"),
+                    Href = new Uri($"{request.Scheme}://{request.Host}/profiles/{username}"),
                 },
                 Link.Create("self") with
                 {
                     Type = "application/activity+json",
-                    Href = new Uri($"https://devtunnel.dark-link.info/profiles/{username}.json"),
+                    Href = new Uri($"{request.Scheme}://{request.Host}/profiles/{username}.json"),
                 }),
         };
         return descriptor;
